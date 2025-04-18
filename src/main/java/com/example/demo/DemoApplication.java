ea4ee1273ee38b2b791625a8267e72e69f2e6877
sandeep-kasturi <124546413+sandeep-kasturi@users.noreply.github.com>
2025-04-18 19:18:42 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 	
 	@GetMapping("/getMsg")
 	public String get() {
-		return "Hello AWS 3";
+		return "Hello AWS 4";
 	}
 	
 	public static void main(String[] args) {
